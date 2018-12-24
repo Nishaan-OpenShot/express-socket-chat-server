6c041837847f1d8612fa3f540df8f269ce7581df
Gurpreet Deol <deol.gs@hotmail.com>
2018-12-24 11:44:54 -0800

    Completetion of First Part

---
+++ config/express-config.js
@@ -0,0 +1,14 @@
+
+class ExpressConfig {
+
+    constructor(app) {
+        // Setting .html as the default template extension
+        app.set('view engine', 'httml');
+
+        // Files
+        app.use(require('express').static(require('path').join('public')));
+
+    }
+
+}
+module.exports = ExpressConfig;
