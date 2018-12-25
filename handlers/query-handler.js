d1a464e22c658fd26b44a82231e42282e9aa8be1
Gurpreet Deol <deol.gs@hotmail.com>
2018-12-24 23:09:28 -0800

    Completion of Third Part

--- handlers/query-handler.js
+++ handlers/query-handler.js
@@ -52,7 +52,7 @@ class Queryhandler {
     userSessionCheck(data) {
         return new Promise( async (resolve, reject) => {
             try {
-                const [DB, ObjectID] = await this.mongodb.onConnect
+                const [DB, ObjectID] = await this.mongodb.onConnect();
                 DB.collection('users').findOne({_id: ObjectID(data.userId), online:'Y'}, (err, result) => {
                     DB.close();
                     if(err) {
@@ -62,6 +62,98 @@ class Queryhandler {
                 });
             } catch(error) {
                 reject(error);
+            }
+        });
+    }
+    
+    getUserInfo({userId,socketId = false}) {
+        let queryProjection = null;
+        if(socketId){
+            queryProjection = {
+                "socketId" : true
+            }
+        } else {
+            queryProjection = {
+                "username" : true,
+                "online" : true,
+                '_id': false,
+                'id': '$_id'
+            }
+        }
+        return new Promise( async (resolve, reject) => {
+            try {
+                const [DB,ObjectID] = await this.Mongodb.onConnect();
+                DB.collection('users').aggregate([{
+                    $match:  {
+                        _id : ObjectID(userId)
+                    }
+                },{
+                    $project : queryProjection
+                }
+                ]).toArray( (err, result) => {
+                    DB.close();
+                    if( err ){
+                        reject(err);
+                    }
+                    socketId ? resolve(result[0]['socketId']) : resolve(result);
+                });
+            } catch (error) {
+                reject(error)
+            }
+        });
+    }
+    
+    addSocketId({userId, socketId}) {
+		const data = {
+			id : userId,
+			value : {
+				$set :{
+					socketId : socketId,
+					online : 'Y'
+				}
+			}
+		};
+		return new Promise( async (resolve, reject) => {
+			try {
+				const [DB, ObjectID] = await this.Mongodb.onConnect();
+				DB.collection('users').update( { _id : ObjectID(data.id)}, data.value ,(err, result) => {
+					DB.close();
+					if( err ){
+						reject(err);
+					}
+					resolve(result);
+				});
+			} catch (error) {
+				reject(error)
+			}
+		});
+    }
+    
+    getChatList(userId){
+        return new Promise( async (resolve, reject) => {
+            try {
+                const [DB,ObjectID] = await this.Mongodb.onConnect();
+                DB.collection('users').aggregate([{
+                    $match: {
+                        'socketId': { $ne : userId}
+                    }
+                },{
+                    $project:{
+                        "username" : true,
+                        "online" : true,
+                        '_id': false,
+                        'id': '$_id'
+                    }
+                }
+                ]).toArray( (err, result) => {
+                    DB.close();
+                    if( err ){
+                        reject(err);
+                    }
+                    resolve(result);
+                });
+            } catch (error) {
+                reject(error)
             }
         });
     }
--- web/socket.js
+++ web/socket.js
@@ -12,12 +12,54 @@ class Socket {
     socketEvents() {
         this.io.on('connection', (socket) => {
 
+            /* Get the user's Chat list	*/
+            socket.on(`chat-list`, async (data) => {
+                if (data.userId == '') {
+                    this.io.emit(`chat-list-response`, {
+                        error : true,
+                        message : CONSTANTS.USER_NOT_FOUND
+                    });
+                } else {
+                    try {
+                        const [UserInfoResponse, chatlistResponse] = await Promise.all([
+                                queryHandler.getUserInfo( {
+                                    userId: data.userId,
+                                    socketId: false
+                                }),
+                                queryHandler.getChatList( socket.id )
+                            ]);
+                        this.io.to(socket.id).emit(`chat-list-response`, {
+                            error : false,
+                            singleUser : false,
+                            chatList : chatlistResponse
+                        });
+                        socket.broadcast.emit(`chat-list-response`,{
+                            error : false,
+                            singleUser : true,
+                            chatList : UserInfoResponse
+                        });
+                    } catch ( error ) {
+                        this.io.to(socket.id).emit(`chat-list-response`,{
+                            error : true ,
+                            chatList : []
+                        });
+                    }
+                }
+            });
         })
     }
 
     socketConfig() {
         this.io.use( async (socket, next) => {
-            // Some code will go here
+            try {
+                await queryHandler.addSocketId({
+                    userId: socket.request._query['userId'],
+                    socketId: socket.id
+                });
+                next();
+            } catch(error) {
+                console.error(error);
+            }
         });
         this.socketEvents();
     }
